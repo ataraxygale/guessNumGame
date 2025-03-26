9a64d46d298e26f58c5211c1f17aad74b04867b2
Angel <94824625+ataraxygale@users.noreply.github.com>
2025-03-26 13:14:00 -0600

    Add files via upload

---
+++ secretNumGame.py
@@ -0,0 +1,41 @@
+secret_number = 777
+
+print(
+    """
+    +========================================+
+    | Welcome to: 🧙‍♂️ SORCERER'S PUZZLE TOWER |
+      What is my secret number?!             |
+    | Enter an integer and guess.            |
+    | Guess wrong: YOU ARE TRAPPED           |
+    | Guess correctly: YOU ARE FREE          |
+    +========================================+
+    """)
+
+
+def INTERNALSCREAMINGBUBBLE():
+    while True:
+        try:
+            return int(input("ENTER A NUMBER: \n"))
+        except ValueError:
+            print("YOU WILL STAY HERE FOREVER. TRY AGAIN MORTAL\n")
+
+
+numGuess = INTERNALSCREAMINGBUBBLE()
+guessCount = 1
+
+"""
+            numGuess = int(input("GUESS MY SECRET NUMBER. MUAHAHA. ENTER A NUMBER BETWEEN 1 AND 1000. *DIABOLICAL CACKLING*\n"))
+"""
+while numGuess != 777:
+    try:
+        numGuess = int(input("Ha ha! You're stuck in my loop FOREVER\n"))
+    except ValueError:
+        print("Invalid input: Please enter an integer FOOL\n")
+    guessCount += 1
+
+print("WELL DONE, FELLOW SORCERER/ESS 🪄 \n")
+
+if guessCount > 5:
+    print(f"You took {guessCount} human attempts. PATHETIC")
+else:
+    print(f"You took {guessCount} human attempt/s. ACCEPTABLE")
