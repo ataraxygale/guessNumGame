0049c88890252485d8043e3e6ea62157f8fdd0e4
Angel <94824625+ataraxygale@users.noreply.github.com>
2025-03-26 13:49:54 -0600

    Update secretNumGame.py

--- secretNumGame.py
+++ secretNumGame.py
@@ -1,41 +1,54 @@
-secret_number = 777
+def secretNumGame():
+    secret_number = 777
 
-print(
+    print(
+        """
+        +========================================+
+        | Welcome to: 🧙‍♂️ SORCERER'S PUZZLE TOWER |
+          What is my secret number?!             |
+        | Enter an integer and guess.            |
+        | Guess wrong: YOU ARE TRAPPED           |
+        | Guess correctly: YOU ARE FREE          |
+        +========================================+
+        """)
+
+
+    def INTERNALSCREAMINGBUBBLE():
+        while True:
+            try:
+                return int(input("ENTER A NUMBER: \n"))
+            except ValueError:
+                print("YOU WILL STAY HERE FOREVER. TRY AGAIN MORTAL\n")
+
+
+    numGuess = INTERNALSCREAMINGBUBBLE()
+    guessCount = 1
+
     """
-    +========================================+
-    | Welcome to: 🧙‍♂️ SORCERER'S PUZZLE TOWER |
-      What is my secret number?!             |
-    | Enter an integer and guess.            |
-    | Guess wrong: YOU ARE TRAPPED           |
-    | Guess correctly: YOU ARE FREE          |
-    +========================================+
-    """)
-
-
-def INTERNALSCREAMINGBUBBLE():
-    while True:
+                numGuess = int(input("GUESS MY SECRET NUMBER. MUAHAHA. ENTER A NUMBER BETWEEN 1 AND 1000. *DIABOLICAL CACKLING*\n"))
+    """
+    while numGuess != 777:
         try:
-            return int(input("ENTER A NUMBER: \n"))
+            numGuess = int(input("Ha ha! You're stuck in my loop FOREVER\n"))
         except ValueError:
-            print("YOU WILL STAY HERE FOREVER. TRY AGAIN MORTAL\n")
+            print("Invalid input: Please enter an integer FOOL\n")
+        guessCount += 1
 
+    print("WELL DONE, FELLOW SORCERER/ESS 🪄 \n")
 
-numGuess = INTERNALSCREAMINGBUBBLE()
-guessCount = 1
+    if guessCount > 5:
+        print(f"You took {guessCount} human attempts. PATHETIC")
+    else:
+        print(f"You took {guessCount} human attempt/s. ACCEPTABLE")
 
-"""
-            numGuess = int(input("GUESS MY SECRET NUMBER. MUAHAHA. ENTER A NUMBER BETWEEN 1 AND 1000. *DIABOLICAL CACKLING*\n"))
-"""
-while numGuess != 777:
-    try:
-        numGuess = int(input("Ha ha! You're stuck in my loop FOREVER\n"))
-    except ValueError:
-        print("Invalid input: Please enter an integer FOOL\n")
-    guessCount += 1
+    YN = input("Would you like to play again? Y/N\n").strip().lower()
 
-print("WELL DONE, FELLOW SORCERER/ESS 🪄 \n")
+    if YN == 'y':
+        secretNumGame()
+    else:
+        print("\nFarewell, mortal")
+        exit()
 
-if guessCount > 5:
-    print(f"You took {guessCount} human attempts. PATHETIC")
-else:
-    print(f"You took {guessCount} human attempt/s. ACCEPTABLE")
+if __name__ == "__main__":
+    secretNumGame()
+    
